547923a3e36c5e010390e78d29b032ada8fc85b5
LOL-Hunter <72355355+LOL-Hunter@users.noreply.github.com>
2024-06-19 11:01:10 +0200

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -2,10 +2,14 @@ from gui import Window
 from os import _exit
 from pysettings.text import Title, Color, MsgText
 
-if __name__ == '__main__':
+"""
+Main-Script
+This script starts the Hypixel-Tools software.
+"""
 
+if __name__ == '__main__':
     Title().print("Sky Block Tools", Color.GREEN)
     window = Window()
     MsgText.info("GUI opened successfully!")
     window.mainloop()
-    _exit(0)
+    _exit(0) # quit all threads
